747d138ebcc582948125e920cadf88b3f670e689
SmallQ008 <71476570+SmallQ008@users.noreply.github.com>
2021-04-20 15:20:46 +0800

    123

---
+++ Assets/腳本/player.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+[Header("等級")]
+    public int lv = 1;
+    [Range(0,300)]
+    public float speed = 10.5f;
+    public bool isDead = false;
+    [Tooltip("這是角色的名稱")]
+    public string cName = "貓咪";
+
+
+    private void Move()
+    {
+
+    }
+
+    private void Attack()
+    {
+ 
+    }
+
+    private void Hit()
+    { 
+    
+    }
+
+    private void Dead()
+    { 
+    
+    }
+
+}
