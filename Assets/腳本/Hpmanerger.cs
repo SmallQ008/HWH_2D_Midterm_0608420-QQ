c203b7b220f5389ecba8a502616ad98da06918f5
SmallQ008 <71476570+SmallQ008@users.noreply.github.com>
2021-05-11 01:49:15 +0800

    123

--- Assets/腳本/Hpmanerger.cs
+++ Assets/腳本/Hpmanerger.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
-
-public class Hpmanerger : MonoBehaviour
+using UnityEngine.UI;
+using System.Collections;
+public class HPmaneger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("血條")]
+    public Image bar;
+    [Header("傷害數值")]
+    public RectTransform rectDamage;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="Hp">當前血量</param>
+    /// <param name="HpMax">血量最大值</param>
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateHpBar(float Hp, float HpMax)
     {
-        
+        bar.fillAmount = Hp / HpMax;
+    }
+    public IEnumerator ShowDamage(float damage)
+    {
+        RectTransform rect = Instantiate(rectDamage, transform);
+        rect.anchoredPosition = new Vector2(0, 200);
+        rect.GetComponent<Text>().text = damage.ToString();
+
+        float y = rect.anchoredPosition.y;
+
+        while (y < 400)
+        {
+            y += 20;
+            rect.anchoredPosition = new Vector2(0, y);
+            yield return new WaitForSeconds(0.02f);
+        }
+        Destroy(rect.gameObject, 0.5f);
     }
 }
